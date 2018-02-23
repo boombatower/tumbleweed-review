b2ea2daafd3293f1a878a786e31426c8efcf086f
Jimmy Berry <jimmy@boombatower.com>
2018-02-22 23:54:26 -0600

    score: take into account all other date sources and previous impacts.

--- src/score.py
+++ src/score.py
@@ -5,12 +5,6 @@ from util.common import release_to_date
 from util.common import yaml_load
 import yaml
 
-def mail_score(mail_release):
-    impact = 0
-    for thread in mail_release['threads']:
-        impact += max(min(thread['reference_count'], 25), 3)
-    return impact
-
 def stability_level(release, score):
     release_date = release_to_date(release)
     if (date.today() - release_date).days < 7:
@@ -26,13 +20,59 @@ def stability_level(release, score):
         return 'moderate';
     return 'unstable'
 
-def score(mail):
+def bug_score(bugs):
+    return len(bugs) / 10
+
+def mail_score(mail_release):
+    impact = 0
+    for thread in mail_release['threads']:
+        impact += max(min(thread['reference_count'], 25), 3)
+    return impact
+
+def snapshot_score(snapshot):
+    if not snapshot:
+        return 10
+
+    impact = 0
+
+    binary_interest = snapshot['binary_interest']
+    binary_interest_changed = snapshot['binary_interest_changed']
+
+    parts = binary_interest['kernel-source'].split('.')
+    if len(parts) == 3:
+        minor = int(parts[2])
+        if minor <= 1:
+            impact += 15
+        elif minor <= 3:
+            impact += 10
+
+    if 'Mesa' in binary_interest_changed:
+        parts = binary_interest['Mesa'].split('.')
+        if len(parts) == 3:
+            minor = int(parts[2])
+            if minor == 0:
+                impact += 5
+
+    if snapshot['binary_unique_count'] / snapshot['binary_count'] > 0.35:
+        impact += 10
+
+    return impact
+
+def score(bugs, mail, snapshot):
     scores = {}
 
+    impact_previous = None
     for release, mail_release in mail.items():
-        score = 100
-        score -= mail_score(mail_release)
+        impact = 0
+        impact += bug_score(bugs.get(release, []))
+        impact += mail_score(mail_release)
+        impact += snapshot_score(snapshot.get(release, None))
 
+        if impact_previous:
+            impact += 0.40 * impact_previous
+        impact_previous = impact
+
+        score = 100 - impact
         scores[release] = int(score)
 
     return scores
@@ -43,8 +83,10 @@ def main(logger_, data_dir):
 
     ensure_directory(data_dir)
 
+    bugs = yaml_load(data_dir, 'bug.yaml')
     mail = yaml_load(data_dir, 'mail.yaml')
-    scores = score(mail)
+    snapshot = yaml_load(data_dir, 'snapshot.yaml')
+    scores = score(bugs, mail, snapshot)
 
     with open(path.join(data_dir, 'score.yaml'), 'w') as outfile:
         yaml.safe_dump(scores, outfile, default_flow_style=False)
